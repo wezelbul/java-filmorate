b7d5f0daec6d304fb90d3ac9d1404a7dd6de1a2e
Dmitriy Chesnokov <chesnokov.dmitry.64@gmail.com>
2022-08-07 20:47:58 +0400

    feat: ТЗ-9 ControllerAdvice и вспомогательные классы для обработки исключений

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/advice/Response.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.controller.advice;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Response {
+    private final String message;
+}
