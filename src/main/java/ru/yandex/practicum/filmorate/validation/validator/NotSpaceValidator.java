46b096d9564c274bf55adf76b8920b3d69b39672
Dmitriy Chesnokov <chesnokov.dmitry.64@gmail.com>
2022-08-08 13:12:02 +0400

    refactor: упростил проверку условий стандартными методами фреймворка Spring

--- src/main/java/ru/yandex/practicum/filmorate/validation/validator/NotSpaceValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/validator/NotSpaceValidator.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.validation.validator;
 
+import org.springframework.util.StringUtils;
 import ru.yandex.practicum.filmorate.validation.annotation.NotSpace;
 
 import javax.validation.ConstraintValidator;
@@ -9,8 +10,8 @@ public class NotSpaceValidator implements ConstraintValidator<NotSpace, String> 
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        if (s != null && !s.isEmpty() && !s.isBlank()) {
-            return !s.contains(" ");
+        if (StringUtils.hasText(s)) {
+            return !StringUtils.containsWhitespace(s);
         }
         return true;
     }
