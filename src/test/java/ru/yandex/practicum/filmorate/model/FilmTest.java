127eca0f636a0ad69af8d686ce9edd6e4657283a
Dmitriy Chesnokov <chesnokov.dmitry.64@gmail.com>
2022-08-17 18:50:34 +0400

    rename

--- src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import org.junit.jupiter.api.Test;
 
-public class FilmTest extends AbstractDataObjectTest<Film> {
+public class FilmTest extends AbstractDataModelTest<Film> {
 
     String wrongJsonDir = super.jsonDir + "/wrong/model/film";
     String correctJsonDir = super.jsonDir + "/correct/model/film";
--- src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import org.junit.jupiter.api.Test;
 
-public class UserTest extends AbstractDataObjectTest<User> {
+public class UserTest extends AbstractDataModelTest<User> {
 
     String wrongJsonDir = super.jsonDir + "/wrong/model/user";
     String correctJsonDir = super.jsonDir + "/correct/model/user";
