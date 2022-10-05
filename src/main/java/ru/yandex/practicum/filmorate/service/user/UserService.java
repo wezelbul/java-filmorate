c78d72bb4e4607a6f5b019dfc50733731d3491c7
ViktorKorol <viktor.korol31@yandex.ru>
2022-10-05 11:22:19 +0600

    fix: Функциональность «Рекомендации»

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.service.user;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.base.DataObjectNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.base.data.AbstractDataService;
+import ru.yandex.practicum.filmorate.storage.film.DbFilmStorage;
 import ru.yandex.practicum.filmorate.storage.friend.DbFriendStorage;
 import ru.yandex.practicum.filmorate.storage.friend.FriendStorage;
 import ru.yandex.practicum.filmorate.storage.like.LikeStorage;
@@ -16,12 +18,17 @@ import java.util.*;
 public class UserService extends AbstractDataService<User, DbUserStorage> {
 
     private final FriendStorage friendStorage;
+    private final DbFilmStorage filmStorage;
     private final LikeStorage likeStorage;
     private final Integer defaultCountPopularUsers = 10;
 
-    public UserService(DbUserStorage userStorage, DbFriendStorage friendStorage,LikeStorage likeStorage) {
+    public UserService(DbUserStorage userStorage,
+                       DbFriendStorage friendStorage,
+                       DbFilmStorage filmStorage,
+                       LikeStorage likeStorage) {
         super(userStorage);
         this.friendStorage = friendStorage;
+        this.filmStorage = filmStorage;
         this.likeStorage=likeStorage;
     }
 
@@ -77,4 +84,16 @@ public class UserService extends AbstractDataService<User, DbUserStorage> {
         return super.delete(userId);
     }
 
+    // список фильмов рекомендуемые пользователю
+    public List<Film> getUsersRecommendations(Long id) {
+        List<Long> recommendUserFilms = filmStorage.getUsersRecommendations(id);
+        List<Long> userFilms = filmStorage.getFilmsUserById(id);
+        recommendUserFilms.removeAll(userFilms);
+        List<Film> recommendFilms = new ArrayList<>();
+
+        for (Long indexFilm:recommendUserFilms) {
+            recommendFilms.add(filmStorage.getById(indexFilm));
+        }
+        return recommendFilms;
+    }
 }
