76166dc016b353db98451570181f8e54264102dd
paradizer <Valdemarushka89@yandex.ru>
2022-10-11 00:46:18 +0700

    Еще одно исправление

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -169,7 +169,7 @@ public class FilmService extends AbstractDataService<Film, DbFilmStorage> {
     public List<Film> getMostCommonFilms(Long userId, Long friendId) {
         return likeStorage.getMostCommonFilms(userId, friendId);
     }
-    
+
     public boolean deleteFilm(Long filmId) {
         if (!contains(filmId)) {
             throw new DataObjectNotFoundException(filmId);
@@ -225,6 +225,5 @@ public class FilmService extends AbstractDataService<Film, DbFilmStorage> {
             }
         }
         return filmsByDirector;
-
-
+    }
 }
